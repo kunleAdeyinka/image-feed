f81706d33f233cf67254b8123cc6bd38ea49c0fe
Kunle Adeyinka <aadeyink@gmail.com>
2018-12-22 06:43:44 -0500

    First Commit
    This is the first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Constants } from 'expo';
+
+import Card from './components/Card';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Card 
+          fullname={'First Last'} 
+          linkText={'Comments'} 
+          onPressLinkText={() => {
+            console.log('Pressed link!');
+          }}
+          image={{ uri: 'https://unsplash.it/600/600' }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
